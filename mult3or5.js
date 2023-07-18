f7db66ae5dc3c6689219a4227d9895a939514d8d
BeedinM <douglas.bedin@gmail.com>
2023-07-18 11:45:58 -0300

    add comentário

--- mult3or5.js
+++ mult3or5.js
@@ -1,11 +1,11 @@
 function solution(number){
-
+    //verifico se o número é negativo.
     if(number < 0) {
       return 0;
     }
-  
+  //crio variável para conter o total da soma.
   let totalSum = 0;
-  
+  //loop para verificar número por número se é multiplicável.
   for(let num = 1; num < number; num++){
     if (num % 3 === 0 || num % 5 === 0) {
       totalSum += num;
